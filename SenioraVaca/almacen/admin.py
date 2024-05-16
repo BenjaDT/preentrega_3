a7f5e59dd0f241c254db5870fec6aa8fa7a9a1f0
Benjamin Di Tella <benja.m.dite@gmail.com>
2024-05-16 17:08:44 -0300

    crud funcionando

--- SenioraVaca/almacen/admin.py
+++ SenioraVaca/almacen/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from . import models
+
+admin.site.register(models.Persona)
+admin.site.register(models.Provedor)
+admin.site.register(models.Posicion)
+admin.site.register(models.Producto)
+admin.site.register(models.Trabajador)
