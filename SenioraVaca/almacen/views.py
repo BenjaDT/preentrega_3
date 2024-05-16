ff534c4f49c574604014e0e3c67d9ad674deaab0
Benjamin Di Tella <benja.m.dite@gmail.com>
2024-05-16 18:59:54 -0300

    urls andando

--- SenioraVaca/almacen/views.py
+++ SenioraVaca/almacen/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from . import models
 # Create your views here.
+
+def index(request):
+    consulta =models.Producto.objects.all()
+    contexto = {'productos':consulta}
+    return render(request,'Almacen/index.html', contexto)
